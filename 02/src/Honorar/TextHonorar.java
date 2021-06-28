0f9ffc0e47eced5c566a78c148b3cdd07ec43e1f
George <samis@neutral.gr>
2021-06-28 14:39:26 +0200

    added calcHonorar

--- 02/src/Honorar/TextHonorar.java
+++ 02/src/Honorar/TextHonorar.java
@@ -1,5 +1,25 @@
 package Honorar;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class TextHonorar
 {
+    String text = "";
+
+    public TextHonorar(String text)
+    {
+        this.text = text;
+    }
+
+    public int calcHonorar()
+    {
+        int honorar = 0;
+        honorar += text.length();
+        honorar += text.chars().reduce(0, (subtotal, c) -> subtotal += c == '\n' ? 1 : 0);
+        honorar += text.chars().reduce(0, (subtotal, c) -> subtotal += c == '.' ? 2 : 0);
+        honorar += text.chars().reduce(0, (subtotal, c) -> subtotal += c == '!' ? 3 : 0);
+
+        return honorar;
+    }
 }
