c95287dcadaf82fbe55975dc3ca7b674735d9b8b
George <samis@neutral.gr>
2021-06-28 14:38:00 +0200

    added calcHonorar Test

---
+++ 02/src/Honorar/TextHonorarTest.java
@@ -0,0 +1,26 @@
+package Honorar;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TextHonorarTest
+{
+
+    @Test
+    public void calcHonorar()
+    {
+        String text = "Hallo";
+        String text2 = "Hallo!";
+        String text3 = "Hallo.!";
+
+        TextHonorar textHonorar = new TextHonorar(text);
+        assertEquals(5, textHonorar.calcHonorar());;
+        textHonorar.setText(text2);
+        assertEquals(9, textHonorar.calcHonorar());;
+        textHonorar.setText(text3);
+        assertEquals(12, textHonorar.calcHonorar());;
+    }
+
+
+}
