30fd0e043e41bb968e3300b64c1a232f66ebe7dd
George <samis@neutral.gr>
2021-07-04 15:09:28 +0200

    added tests junit 4

---
+++ 03/src/test/java/Birthday/BirthdayCalcTest.java
@@ -0,0 +1,48 @@
+package Birthday;
+
+import org.junit.Test;
+
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.*;
+
+public class BirthdayCalcTest
+{
+
+    @Test
+    public void getAge()
+    {
+        BirthdayCalc bc = new BirthdayCalc(5, 11, 2000);
+        assertEquals(20, bc.getAge());
+    }
+
+    @Test
+    public void getDaysUntilNextBirthday()
+    {
+        BirthdayCalc bc = new BirthdayCalc(5, 11, 2000);
+        assertEquals(124, bc.getDaysUntilNextBirthday());       // needs to be updated every day
+    }
+
+    @Test(expected = DateTimeException.class)
+    public void invalidFormat()
+    {
+        // invalid month
+        BirthdayCalc bc = new BirthdayCalc(5, 13, 2000);
+    }
+
+    @Test
+    public void mockTest()
+    {
+        BirthdayCalc bc = new BirthdayCalc(1, 1, 2000);
+        bc.setBirthday(mockedEinsteinBirthday());
+        assertEquals(142, bc.getAge());
+    }
+
+    LocalDateTime mockedEinsteinBirthday()
+    {
+        return LocalDateTime.of(1879, 3, 14, 0, 0);
+    }
+
+
+}
